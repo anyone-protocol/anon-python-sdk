5c239888b5dd151df77fdea0ab24504a8233926c
web3yurii <web3yurii@gmail.com>
2025-02-18 13:25:31 +0200

    ANON-268 - Add set conf, set stream, attach stream ,filter eit countries

--- anon_python_sdk/control.py
+++ anon_python_sdk/control.py
@@ -1,12 +1,16 @@
 from __future__ import annotations
 
-from typing import List, Optional, Sequence, Tuple, Union
+from typing import List, Optional, Sequence, Tuple, Union, Mapping, Callable, Awaitable
 
 import stem.control
 from stem.descriptor.router_status_entry import RouterStatusEntryV3
-from stem.response.events import CircuitEvent
+from stem.response.events import CircuitEvent, StreamEvent
 
-from .models import Circuit, Hop, Relay
+# temp
+from stem.response.events import Event
+from stem.control import EventType
+
+from .models import Circuit, Hop, Relay, Stream
 
 
 class Control():
@@ -51,8 +55,44 @@ class Control():
             relay)
         return self._to_relay(router_status)
 
-    def get_country(self, address: Optional[str] = None) -> str:
-        return self._controller.get_info(f'ip-to-country/{address}')
+    def get_network_statuses(self, relays: Optional[Sequence[str]] = None) -> List[Relay]:
+        router_statuses: List[RouterStatusEntryV3] = self._controller.get_network_statuses(
+            relays)
+        return self._to_relays(router_statuses)
+
+    def get_streams(self) -> List[Stream]:
+        stream_events: List[StreamEvent] = self._controller.get_streams()
+        streams = self._to_streams(stream_events)
+        return streams
+
+    def get_stream(self, stream_id: int) -> Optional[Stream]:
+        streams = self.get_streams()
+        for stream in streams:
+            if stream.id == stream_id:
+                return stream
+        return None
+
+    def attach_stream(self, stream_id, circuit_id, exiting_hop=None):
+        self._controller.attach_stream(stream_id, circuit_id, exiting_hop)
+
+    # type: ignore
+    def add_event_listener(self, listener: Callable[[Event], Union[None, Awaitable[None]]], *events: EventType) -> None:
+        self._controller.add_event_listener(listener, *events)
+
+    def remove_event_listener(self, listener: Callable[[Event], Union[None, Awaitable[None]]]) -> None:
+        self._controller.remove_event_listener(listener)
+
+    def set_conf(self, param: str, value: Union[str, Sequence[str]]) -> None:
+        self.set_options({param: value}, False)
+
+    def reset_conf(self, params: str):
+        self.set_options(Mapping([(entry, None) for entry in params]), True)
+
+    def set_options(self, params: Union[Mapping[str, Union[str, Sequence[str]]], Sequence[Tuple[str, Union[str, Sequence[str]]]]], reset: bool = False) -> None:
+        self._controller.set_options(params, reset)
+
+    def get_info(self, params: Union[str, Sequence[str]]) -> Union[str, Mapping[str]]:
+        return self._controller.get_info(params)
 
     def _to_circuits(self, circuit_events: List[CircuitEvent]) -> List[Circuit]:
         return [self._to_circuit(circuit_event) for circuit_event in circuit_events]
@@ -70,6 +110,9 @@ class Control():
             nickname=hop[1],
         )
 
+    def _to_relays(self, router_statuses: List[RouterStatusEntryV3]) -> List[Relay]:
+        return [self._to_relay(router_status) for router_status in router_statuses]
+
     def _to_relay(self, router_status: RouterStatusEntryV3) -> Relay:
         return Relay(
             fingerprint=router_status.fingerprint,
@@ -79,3 +122,31 @@ class Control():
             flags=router_status.flags,
             bandwidth=router_status.bandwidth,
         )
+
+    def _to_streams(self, stream_events: List[StreamEvent]) -> List[Stream]:
+        return [self._to_stream(stream_event) for stream_event in stream_events]
+
+    def _to_stream(self, stream_event: StreamEvent) -> Stream:
+        return Stream(
+            id=stream_event.id,
+            target=stream_event.target,
+        )
+
+    # useful methods
+
+    def get_country(self, address: str) -> str:
+        return self.get_info(f'ip-to-country/{address}')
+
+    def disable_stream_attachment(self):
+        self.set_conf('__LeaveStreamsUnattached', '1')
+
+    def enable_stream_attachment(self):
+        self.reset_conf('__LeaveStreamsUnattached')
+
+    def get_exit_relays(self) -> List[Relay]:
+        relays = self.get_network_statuses()
+        return [relay for relay in relays if 'Exit' in relay.flags]
+
+    def get_exit_relays_by_country(self, _country: str) -> List[Relay]:
+        exit_relays = self.get_exit_relays()
+        return [relay for relay in exit_relays if self.get_country(relay.address) == _country]
---
+++ examples/two_hop_vpn.py
@@ -0,0 +1,44 @@
+from anon_python_sdk import Control, Process, Config
+
+print("Starting Anon...")
+# Create a configuration
+config = Config(
+    auto_terms_agreement=True,
+    display_log=False
+)
+
+# Initialize and start the runner
+anon = Process.launch_anon(anonrc_path=config.to_file())
+
+control = Control.from_port()
+
+try:
+    print("Connecting to Control Port...")
+    control.authenticate()
+
+    exits = control.get_exit_relays_by_country("nl")
+
+    print("Exit Relays:", exits)
+
+    # print("Creating a new circuit through specified relays...")
+    # # relays = [
+    # #     "894D4088C63D3FA4446E505E672C47A8247AC891",
+    # #     "6DB2B0D574CE216648CA388D309EE7CF5DF0B423",
+    # #     "A17C391AAE45689358EC226C43D1290EBED7437A"
+    # #     ]# Replace with real relay fingerprints
+    # m_circuit_id = control.new_circuit(await_build=True)
+    # print(f"New circuit created with ID: {m_circuit_id}")
+
+    # m_circuit = control.get_circuit(m_circuit_id)
+    # print(f"Manual Circuit ID: {m_circuit.id}, Path: {m_circuit.path}")
+
+    # print("Closing the manual circuit...")
+    # control.close_circuit(m_circuit_id)
+    # print(f"Manual circuit {m_circuit_id} closed successfully.")
+
+finally:
+    control.close()
+    print("Controller connection closed.")
+
+    print("Stopping Anon...")
+    anon.stop()
